05ee3d90d6125d3ca3741e91368cc8239b28c859
tom-code <petrilak.tomas@gmail.com>
2018-08-14 20:28:28 +0200

    switched to usart2

--- usart.cc
+++ usart.cc
@@ -12,49 +12,50 @@ static char to_send[100]= "";
 
 void usart_setup() {
   to_send[0] = 0;
+
   rcc_periph_clock_enable(RCC_GPIOA);
   rcc_periph_clock_enable(RCC_AFIO);
-  rcc_periph_clock_enable(RCC_USART1);
+  rcc_periph_clock_enable(RCC_USART2);
 
-  nvic_enable_irq(NVIC_USART1_IRQ);
-  gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX);
-  gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, GPIO_USART1_RX); 
+  nvic_enable_irq(NVIC_USART2_IRQ);
+  gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART2_TX);
+  gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, GPIO_USART2_RX); 
 
-  usart_set_baudrate(USART1, 9600);
-  usart_set_databits(USART1, 8);
-  usart_set_stopbits(USART1, USART_STOPBITS_1);
-  usart_set_parity(USART1, USART_PARITY_NONE);
-  usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
-  usart_set_mode(USART1, USART_MODE_TX_RX); 
+  usart_set_baudrate(USART2, 9600);
+  usart_set_databits(USART2, 8);
+  usart_set_stopbits(USART2, USART_STOPBITS_1);
+  usart_set_parity(USART2, USART_PARITY_NONE);
+  usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+  usart_set_mode(USART2, USART_MODE_TX_RX); 
 
-  USART_CR1(USART1) |= USART_CR1_RXNEIE;
-  usart_enable(USART1);
+  USART_CR1(USART2) |= USART_CR1_RXNEIE;
+  usart_enable(USART2);
 }
 
 void usart1_send(const char *data) {
   strcpy(to_send, data);
-  USART_CR1(USART1) |= USART_CR1_TXEIE;
+  USART_CR1(USART2) |= USART_CR1_TXEIE;
 }
 
-extern "C" void usart1_isr(void)
+extern "C" void usart2_isr(void)
 {
   /* Check if we were called because of RXNE. */
-  if (((USART_CR1(USART1) & USART_CR1_RXNEIE) != 0) && ((USART_SR(USART1) & USART_SR_RXNE) != 0)) {
+  if (((USART_CR1(USART2) & USART_CR1_RXNEIE) != 0) && ((USART_SR(USART2) & USART_SR_RXNE) != 0)) {
 
-    /*data = */usart_recv(USART1);
+    /*data = */usart_recv(USART2);
     usart1_send("don't disturb\n\r");
   }
 
   /* Check if we were called because of TXE. */
-  if (((USART_CR1(USART1) & USART_CR1_TXEIE) != 0) && ((USART_SR(USART1) & USART_SR_TXE) != 0)) {
+  if (((USART_CR1(USART2) & USART_CR1_TXEIE) != 0) && ((USART_SR(USART2) & USART_SR_TXE) != 0)) {
 
     if (strlen(to_send) > 0) {
-      usart_send(USART1, to_send[0]);
+      usart_send(USART2, to_send[0]);
       memmove(to_send, to_send+1, strlen(to_send)+1);
     }
 
     /* Disable the TXE interrupt as we don't need it anymore. */
-    if (strlen(to_send) == 0) USART_CR1(USART1) &= ~USART_CR1_TXEIE;
+    if (strlen(to_send) == 0) USART_CR1(USART2) &= ~USART_CR1_TXEIE;
   }
 }
 
